972aa1d44a67e02ee81ed3bac84ce48d27ff9769
loucims <sebastian.loucim@gmail.com>
2024-06-21 17:10:50 -0300

    Connected game to backend!

--- screens/ChooseCategory.js
+++ screens/ChooseCategory.js
@@ -1,13 +1,58 @@
 
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 const ChooseCategory = ({ navigation }) => {
+
+    const [loading, setLoading] = useState(false)
+
+    const fetchQuestions = async (genre = 'movie', pageSize = 5, lastKey = '') => {
+        setLoading(true)
+        try {
+            const fetchOptions = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+    
+            const params = new URLSearchParams({
+                pageSize: pageSize,
+                lastKey: lastKey
+            });
+    
+            const response = await fetch(`http://10.0.2.2:8080/options/genre/${genre}/paginated?${params}`, fetchOptions);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result = await response.json();
+            console.log(result)
+            return result.message
+            // setOptionFeed(prev => [...prev, result.message.data]);
+        } catch (error) {
+            console.log(error);
+        }
+        setLoading(false)
+    }
+
+    const handleGenrePress = async (genre) => {
+        const result = await fetchQuestions(genre, 5, '')
+        if (!result) return console.log('Error fetching data')
+        navigation.navigate('Game', {
+            genre: genre,
+            limit: 10,
+            startAfterKey: result.lastKey,
+            initData: result.data
+        })
+    }
+
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Juego</Text>
             <Text>Elegir categoria</Text>
-            <Text onPress={() => {navigation.navigate('Game')}}>Peliculas</Text>
-            <Text onPress={() => {navigation.navigate('Game')}}>Videojuegos</Text>
+            <Text onPress={() => handleGenrePress('movie')}>Peliculas</Text>
+            <Text onPress={() => handleGenrePress('movie')}>Videojuegos</Text>
         </View>
     )
 }
--- screens/Game.js
+++ screens/Game.js
@@ -105,40 +105,86 @@ const styles = StyleSheet.create({
     }
 });
 
-const optionsData = [
-    { id: 1, title: 'Joker', image: 'https://dl.dropboxusercontent.com/scl/fi/3fnxgyyb9er2a32welmgy/joker.png?rlkey=jbnpreb0jgg8nf4xfjv9ho9pe&st=whn9a3az&dl=0', value: 160432, blurhash: 'LMC$=es+00t8~oobDjogX5oen*WB'},
-    { id: 2, title: '2012, el Apocalipsis', image: "https://dl.dropboxusercontent.com/scl/fi/960f9td46l40yleaoydc5/2012.png?rlkey=61zr78arzv7j7ycp4y5ucxd7w&st=nf7yhtv2&dl=0", value: 120000, blurhash: 'LDEyrd-pM{-:_4%MsRxt8^?a%1RP'},
-    { id: 3, title: "Furiosa, a Mad Max Story", image: 'https://dl.dropboxusercontent.com/scl/fi/laormsflj60vylmv2v0oz/furiosa.png?rlkey=qipq9ivzp436uia2s4d8g92cz&st=ufvf176w&dl=0', value: 1330302, blurhash: 'LYHnA@S#%Kozy=ofTIt67gWqNdbb'},
-    { id: 4, title: "Starwars", image: 'https://dl.dropboxusercontent.com/scl/fi/npwizc872dixrax7ad6k4/starwars.png?rlkey=v6zaycllvyi6lrtegp09f6tdj&st=7te7a8gt&dl=0', value: 22323, blurhash: 'LC9tiuIr4o=rKnWArpxY9u$y,kIv'},
-    { id: 5, title: "Garfield", image: 'https://dl.dropboxusercontent.com/scl/fi/bsmkprtinmo741ak22gzj/garfield.png?rlkey=ib171dje8gzjhyxcza4acrjfh&st=81ir73sb&dl=0', value: 34334, blurhash: 'LAR]Jsvj_jj^-.knZ+RS@8OXNZ$M'}
+// const optionsData = [
+//     { id: 1, title: 'Joker', image: 'https://dl.dropboxusercontent.com/scl/fi/3fnxgyyb9er2a32welmgy/joker.png?rlkey=jbnpreb0jgg8nf4xfjv9ho9pe&st=whn9a3az&dl=0', value: 160432, blurhash: 'LMC$=es+00t8~oobDjogX5oen*WB'},
+//     { id: 2, title: '2012, el Apocalipsis', image: "https://dl.dropboxusercontent.com/scl/fi/960f9td46l40yleaoydc5/2012.png?rlkey=61zr78arzv7j7ycp4y5ucxd7w&st=nf7yhtv2&dl=0", value: 120000, blurhash: 'LDEyrd-pM{-:_4%MsRxt8^?a%1RP'},
+//     { id: 3, title: "Furiosa, a Mad Max Story", image: 'https://dl.dropboxusercontent.com/scl/fi/laormsflj60vylmv2v0oz/furiosa.png?rlkey=qipq9ivzp436uia2s4d8g92cz&st=ufvf176w&dl=0', value: 1330302, blurhash: 'LYHnA@S#%Kozy=ofTIt67gWqNdbb'},
+//     { id: 4, title: "Starwars", image: 'https://dl.dropboxusercontent.com/scl/fi/npwizc872dixrax7ad6k4/starwars.png?rlkey=v6zaycllvyi6lrtegp09f6tdj&st=7te7a8gt&dl=0', value: 22323, blurhash: 'LC9tiuIr4o=rKnWArpxY9u$y,kIv'},
+//     { id: 5, title: "Garfield", image: 'https://dl.dropboxusercontent.com/scl/fi/bsmkprtinmo741ak22gzj/garfield.png?rlkey=ib171dje8gzjhyxcza4acrjfh&st=81ir73sb&dl=0', value: 34334, blurhash: 'LAR]Jsvj_jj^-.knZ+RS@8OXNZ$M'},
+// ];
 
-];
 
-
-const Game = ({ navigation }) => {
+const Game = ({ navigation, route }) => {
     const insets = useSafeAreaInsets();
     const topPadding = Platform.OS === 'android' ? StatusBar.currentHeight : insets.top;
+    const [optionHeight, setOptionHeight] = useState(0);
+
+    const [loading, setLoading] = useState(false)
+    const { genre, limit, startAfterKey, initData } = route.params;
+
+    const [lastKey, setLastKey] = useState(startAfterKey);
+    const [optionFeed, setOptionFeed] = useState(initData || []);
+    const [options, setOptions] = useState([optionFeed[0], optionFeed[1]]);
 
     const [highScore, setHighScore] = useState(0);
     const [score, setScore] = useState(0);
 
-    const [options, setOptions] = useState([optionsData[0], optionsData[1]]);
-    const [vsMessage, setVsMessage] = useState('VS');
-    const [vsColor, setVsColor] = useState('black');
 
     const [searchCount, setSearchCount] = useState(0);
     const [showResult, setShowResult] = useState(false);
-    const [optionHeight, setOptionHeight] = useState(0);
+
+    const [vsMessage, setVsMessage] = useState('VS');
+    const [vsColor, setVsColor] = useState('black');
     const topOptionAnim = useRef(new Animated.Value(0)).current;
     const bottomOptionAnim = useRef(new Animated.Value(0)).current;
     const optionsOpacity = useRef(new Animated.Value(1)).current;
 
+    const fetchQuestions = async (genre = 'movie', pageSize = 10, paginationLastKey = '') => {
+        setLoading(true);
+        try {
+            const fetchOptions = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+    
+            const params = new URLSearchParams({
+                pageSize: pageSize,
+                lastKey: paginationLastKey
+            });
+    
+            console.log(`http://10.0.2.2:8080/options/genre/${genre}/paginated?${params}`);
+            const response = await fetch(`http://10.0.2.2:8080/options/genre/${genre}/paginated?${params}`, fetchOptions);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result = await response.json();
+    
+            const newOptions = result?.message?.data ?? [];
+            const newLastKey = result?.message?.lastKey ?? '';
+    
+            console.log(result.message);
+    
+            // If the new options are empty, fetch from the beginning
+            if (newOptions.length === 0) {
+                await fetchQuestions(genre, pageSize); // Recursive call with no lastKey
+            } else {
+                setOptionFeed(prev => [...prev, ...newOptions]);
+                setLastKey(newLastKey);
+            }
+    
+        } catch (error) {
+            console.log(error);
+        }
+        setLoading(false);
+    };
+
     useEffect(() => {
         // Prefetch next image when options change
         const prefetchNextImage = async () => {
-            const currentIndex = optionsData.indexOf(options[1]);
-            const nextIndex = (currentIndex + 1) % optionsData.length;
-            const nextImage = optionsData[nextIndex].image;
+
+            const nextImage = optionFeed[0].image;
 
             if (nextImage) {
                 console.log(nextImage)
@@ -160,7 +206,7 @@ const Game = ({ navigation }) => {
         let count = 0;
         const interval = setInterval(() => {
             if (count < targetValue) {
-                count += Math.ceil(targetValue / 100);
+                count += Math.ceil(targetValue / 10);
                 setSearchCount(count);
             } else {
                 clearInterval(interval);
@@ -169,7 +215,7 @@ const Game = ({ navigation }) => {
                     callback();
                 }
             }
-        }, 10);
+        }, 1);
     };
 
     const handlePress = (guess) => {
@@ -205,11 +251,28 @@ const Game = ({ navigation }) => {
                     useNativeDriver: true,
                 }).start(async () => {
 
-                    const nextOptions = [
+                    const [nextOption, ...remainingOptions] = optionFeed;
+
+                    // Update options
+                    setOptions([
                         options[1],
-                        optionsData[(optionsData.indexOf(options[1]) + 1) % optionsData.length]
-                    ];
-                    setOptions(nextOptions);
+                        nextOption
+                    ]);
+
+                    // Update optionFeed
+                    setOptionFeed(remainingOptions);
+
+                    // Check if optionFeed is too low and fetch more options if needed
+                    if (remainingOptions.length <= 2) {
+                        console.log("Fetching more options, ran out of options")
+                        fetchQuestions(genre, limit, lastKey);
+                    }
+
+                    // const nextOptions = [
+                    //     options[1],
+                    //     optionsData[(optionsData.indexOf(options[1]) + 1) % optionsData.length]
+                    // ];
+                    // setOptions(nextOptions);
 
                     // Reset anim
                     topOptionAnim.setValue(0);
