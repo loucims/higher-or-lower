b5e92cccd16b1a75789910c282eaf5a35fb72210
Juan Martin <juanmartinfrick@gmail.com>
2024-06-23 19:41:18 -0300

    new thingies

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,143 +1,198 @@
+import React, { useState, useEffect, useReducer, useCallback } from 'react';
 import {
-  ScrollView,
-  View,
-  KeyboardAvoidingView,
-  StyleSheet,
-  Button,
-  ActivityIndicator,
-  Alert
+    ScrollView,
+    View,
+    KeyboardAvoidingView,
+    StyleSheet,
+    Button,
+    ActivityIndicator,
+    Alert
 } from 'react-native';
-
 import { LinearGradient } from 'expo-linear-gradient';
 import { useDispatch } from 'react-redux';
+
 import Input from '../components/Input';
+import Card from '../components/Card';
+import * as authActions from '../store/actions/auth';
 
 const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
 
-
-const StartScreen = props => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [isSignup, setIsSignup] = useState(false);
-  const dispatch = useDispatch();
-
-  const [formState, dispatchFormState] = useReducer(formReducer, {
-    inputValues: {
-      email: '',
-      password: ''
-    },
-    inputValidities: {
-      email: false,
-      password: false
-    },
-    formIsValid: false
-
-  });
-
-
-
-  useEffect(() => {
-    if (error) {
-      Alert.alert('Ocurrio un error', error, [{ text: 'Ok' }]);
+const formReducer = (state, action) => {
+    if (action.type === FORM_INPUT_UPDATE) {
+        const updatedValues = {
+            ...state.inputValues,
+            [action.input]: action.value
+        };
+        const updatedValidities = {
+            ...state.inputValidities,
+            [action.input]: action.isValid
+        };
+        let updatedFormIsValid = true;
+        for (const key in updatedValidities) {
+            updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+        }
+        return {
+            formIsValid: updatedFormIsValid,
+            inputValidities: updatedValidities,
+            inputValues: updatedValues
+        };
     }
-  }, [error]);
-
-  const authHandler = async () => {
-    let action;
-    if (isSignup) {
-      action = authActions.signup(
-        formState.inputValues.email,
-        formState.inputValues.password
-      );
-
-    } else {
-      action = authActions.login(
-        formState.inputValues.email,
-        formState.inputValues.password
-      );
-
-    }
-    setError(null);
-    setIsLoading(true);
-    try {
-      await dispatch(action);
-      props.navigation.navigate('Game');
-    } catch (err) {
-      setError(err.message);
-      setIsLoading(false);
-    }
-  };
-
-
-  const inputChangeHandler = useCallback(
-    (inputIdentifier, inputValue, inputValidity) => {
-      dispatchFormState({
-        type: FORM_INPUT_UPDATE,
-        value: inputValue,
-        isValid: inputValidity,
-        input: inputIdentifier
-      });
-    },
-    [dispatchFormState]
-  );
-
-
-  return (
-    <KeyboardAvoidingView
-      behavior="padding"
-      keyboardVerticalOffset={50}
-      style={styles.screen}
-    >
-      <LinearGradient colors={['#ffedff', '#ffe3ff']} style={styles.gradient}>
-        <Card style={styles.loginTextContainer}>
-          <ScrollView>
-            <Input
-              id="email"
-              label="E-Mail"
-              keyboardType="email-address"
-              required
-              email
-              autoCapitalize="none"
-              errorText="Por favor ingrese un E-Mail valido"
-              onInputChange={inputChangeHandler}
-              initialValue=""
-            />
-            <Input
-              id="password"
-              label="Password"
-              keyboardType="default"
-              secureTextEntry
-              required
-              minLength={5}
-              autoCapitalize="none"
-              errorText="Por favor ingrese una clave valida"
-              onInputChange={inputChangeHandler}
-              initialValue=""
-            />
-            <View style={styles.loginButton}>
-              {isLoading ? (
-                <ActivityIndicator size="small" />
-              ) : (
-                <Button
-                  title={isSignup ? 'Sign Up' : 'Login'}
-                  onPress={authHandler}
-                />
-              )}
-            </View>
-            <View style={styles.loginButton}>
-              <Button
-                title={`Switch to ${isSignup ? 'Login' : 'Sign Up'}`}
-                onPress={() => {
-                  setIsSignup(prevState => !prevState);
-                }}
-              />
-            </View>
-          </ScrollView>
-        </Card>
-      </LinearGradient>
-    </KeyboardAvoidingView>
-  );
+    return state;
 };
+
+const LoginScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [isSignup, setIsSignup] = useState(false);
+    const dispatch = useDispatch();
+
+    const [formState, dispatchFormState] = useReducer(formReducer, {
+        inputValues: {
+            email: '',
+            password: ''
+        },
+        inputValidities: {
+            email: false,
+            password: false
+        },
+        formIsValid: false
+        
+    });
+    
+    
+
+    useEffect(() => {
+        if (error) {
+            Alert.alert('Ocurrio un error', error, [{ text: 'Ok' }]);
+        }
+    }, [error]);
+
+    const authHandler = async () => {
+        let action;
+        if (isSignup) {
+            action = authActions.signup(
+                formState.inputValues.email,
+                formState.inputValues.password
+            );
+            
+        } else {
+            action = authActions.login(
+                formState.inputValues.email,
+                formState.inputValues.password
+            );
+           
+        }
+        setError(null);
+        setIsLoading(true);
+        try {
+            await dispatch(action);
+            props.navigation.navigate('LoadingScreen');
+        } catch (err) {
+            setError(err.message);
+            setIsLoading(false);
+        }
+    };
+  
+
+    const inputChangeHandler = useCallback(
+        (inputIdentifier, inputValue, inputValidity) => {
+            dispatchFormState({
+                type: FORM_INPUT_UPDATE,
+                value: inputValue,
+                isValid: inputValidity,
+                input: inputIdentifier
+            });
+        },
+        [dispatchFormState]
+    );
+   
+
+    return (
+        <KeyboardAvoidingView
+            behavior="padding"
+            keyboardVerticalOffset={50}
+            style={styles.screen}
+        >
+            <LinearGradient colors={['#ffedff', '#ffe3ff']} style={styles.gradient}>
+                <Card style={styles.authContainer}>
+                    <ScrollView>
+                        <Input
+                            id="email"
+                            label="E-Mail"
+                            keyboardType="email-address"
+                            required
+                            email
+                            autoCapitalize="none"
+                            errorText="Por favor ingrese un E-Mail valido"
+                            onInputChange={inputChangeHandler}
+                            initialValue=""
+                        />
+                        <Input
+                            id="password"
+                            label="Password"
+                            keyboardType="default"
+                            secureTextEntry
+                            required
+                            minLength={5}
+                            autoCapitalize="none"
+                            errorText="Por favor ingrese una clave valida"
+                            onInputChange={inputChangeHandler}
+                            initialValue=""
+                        />
+                        <View style={styles.buttonContainer}>
+                            {isLoading ? (
+                                <ActivityIndicator size="small"/>
+                            ) : (
+                                <Button
+                                    title={isSignup ? 'Sign Up' : 'Login'}
+                                    onPress={authHandler}
+                                />
+                            )}
+                        </View>
+                        <View style={styles.buttonContainer}>
+                            <Button
+                                title={`Switch to ${isSignup ? 'Login' : 'Sign Up'}`}
+                                onPress={() => {
+                                    setIsSignup(prevState => !prevState);
+                                }}
+                            />
+                        </View>
+                    </ScrollView>
+                </Card>
+            </LinearGradient>
+        </KeyboardAvoidingView>
+    );
+};
+console.log('COMO TERMINO', LoginScreen);
+
+
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    },
+    gradient: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    authContainer: {
+        width: '80%',
+        maxWidth: 400,
+        maxHeight: 400,
+        padding: 20
+    },
+    buttonContainer: {
+        marginTop: 10
+    }
+});
+
+export default LoginScreen;
+
+
+
+
+
 
 /*export default function LoginScreen({navigation}) {
     return (
@@ -172,7 +227,7 @@ const StartScreen = props => {
   }*/
 
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'column',
@@ -240,4 +295,4 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   }
-});
+});*/
